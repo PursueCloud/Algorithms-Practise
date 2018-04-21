1135c464c3f042e5989a41e959320b418d094daf
PursueCloud <yingzzzyue@gmail.com>
2018-04-21 21:22:52 +0800

    update：1、完善两个栈组成的队列注意事项

--- src/com/yo/algorithm/practise/cig/stackqueue/twostackasqueue2/TestTwoStackAsQueue.java
+++ src/com/yo/algorithm/practise/cig/stackqueue/twostackasqueue2/TestTwoStackAsQueue.java
@@ -17,7 +17,7 @@ public class TestTwoStackAsQueue {
             6 5 4 3 2 1
         总结：
             实现原理：一个栈stackIn负责压入数据，一个栈stackOut负责取出数据，取出数据之前必须把stackIn中的数据压进stackOut中。由于栈的特点是先进先出，所以当stackIn中的数据压入stackOut中之后，数据取出顺序会变成鱼压入的顺序一样，即先进先出
-            注意事项：stackIn中的数据必须一次性压入stackOut中，即当stackOut不为空时，stackIn不能向其中压入数据，否则会出现数据取出顺序错误
+            注意事项：1、stackIn中的数据必须一次性压入stackOut中；2、当stackOut不为空时，stackIn不能向其中压入数据。否则会出现数据取出顺序错误
             （错误举例：
                 1、数据取出顺序不能保证跟其压入顺序一样，比如当压入1到stackIn时，stackOut也压入了1；压入2到stackIn时，stackOut也压入了2。此时从stackOut中取出数据顺序就变成了：2，1；
                 2、一次性压入1，2，3到stackIn，压入完成后stackOut一次性压入3，2，1；然后stackIn再一次性压入4，5，6，stackOut再一次性压入6，5，4。此时，从stackOut中取出数据顺序就变成了：4，5，6，1，2，3
