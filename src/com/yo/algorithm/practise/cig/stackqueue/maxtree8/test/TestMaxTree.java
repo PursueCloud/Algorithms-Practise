f33f324bb2195436999038462faeb3743664356e
PursueCloud <yingzzzyue@gmail.com>
2018-05-22 17:55:17 +0800

    update：1、添加数组最大生成树问题练习和分析

--- src/com/yo/algorithm/practise/cig/stackqueue/maxtree8/test/TestMaxTree.java
+++ src/com/yo/algorithm/practise/cig/stackqueue/maxtree8/test/TestMaxTree.java
@@ -1,12 +1,29 @@
 package com.yo.algorithm.practise.cig.stackqueue.maxtree8.test;
 
-import com.yo.algorithm.practise.cig.stackqueue.maxtree8.impl.MaxTree;
 import com.yo.algorithm.practise.cig.stackqueue.maxtree8.impl.structure.Node;
 
 public class TestMaxTree {
+    /**
+     * 测试结果（{3, 4, 5, 1, 2}）：
+     *      5
+     *    /  |
+     *   4    2
+     *  /    /
+     * 3    1
+     * 测试结果（{3, 1, 2}）：
+     *      3
+     *    /
+     *   2
+     *  /
+     * 1
+     * @param args
+     */
     public static void main(String[] args) {
-        int[] arr = {3, 4, 5, 1, 2};
-        Node maxTree = MaxTree.getMaxTree(arr);
-        System.out.println(maxTree);
+        int[] arr1 = {3, 4, 5, 1, 2};
+        Node maxTree1 = com.yo.algorithm.practise.cig.stackqueue.practise.MaxTree.getMaxTree(arr1);
+        System.out.println(maxTree1);
+        int[] arr2 = {3, 1, 2};
+        Node maxTree2 = com.yo.algorithm.practise.cig.stackqueue.practise.MaxTree.getMaxTree(arr2);
+        System.out.println(maxTree2);
     }
 }
